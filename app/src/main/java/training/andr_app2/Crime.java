7d352d644995a1340cc80fee4cc55a3d06bbadc7
nomad9000 <nomad9000@mail.ru>
2017-01-18 16:38:48 +0500

    init

---
+++ app/src/main/java/training/andr_app2/Crime.java
@@ -0,0 +1,29 @@
+package training.andr_app2;
+
+import java.util.UUID;
+
+/**
+ * Created by Kudryashov_D on 18.01.2017.
+ */
+
+public class Crime {
+
+    private UUID mId;
+    private String mTitle;
+
+    public Crime() {
+        mId = UUID.randomUUID();
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+}
---
+++ app/src/main/java/training/andr_app2/CrimeFragment.java
@@ -0,0 +1,53 @@
+package training.andr_app2;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.EditText;
+import android.widget.ListAdapter;
+
+/**
+ * Created by Kudryashov_D on 18.01.2017.
+ */
+
+public class CrimeFragment extends Fragment {
+
+    private Crime mCrime;
+    private EditText mTitleField;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mCrime = new Crime();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        //super.onCreateView(inflater, container, savedInstanceState);
+        View v = inflater.inflate(R.layout.fragment_crime, container, false);
+        mTitleField = (EditText) v.findViewById(R.id.crime_title);
+        mTitleField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence c, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence c, int start, int before, int count) {
+                mCrime.setTitle(c.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable c) {
+
+            }
+        });
+        return v;
+    }
+}
